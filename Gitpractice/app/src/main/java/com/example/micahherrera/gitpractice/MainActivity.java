f420fd729ab7d42bc414d0cf806c4a6b2928abd5
Micah Herrera <micah@micahherrera.com>
2016-06-01 21:21:01 -0500

    mypart2

---
+++ Gitpractice/app/src/main/java/com/example/micahherrera/gitpractice/MainActivity.java
@@ -0,0 +1,31 @@
+
+package ly.generalassemb.githublab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.micahherrera.gitpractice.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
+public class Car.java{
+        String make;
+        String model;
+        String engine;
+
+        }
+public class Main.java{
+public static void main
+
+        }
+
+public class Garage.java{}
